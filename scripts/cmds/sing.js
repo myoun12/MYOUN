9da48fd7f80cf4abae607b050004396500877544
myoun12 <mdmyoun256@gmail.com>
2026-08-08 12:16:58 +0600

    Update sing.js

--- scripts/cmds/sing.js
+++ scripts/cmds/sing.js
@@ -4,7 +4,7 @@ module.exports = {
         config: {
                 name: "sing",
                 version: "4.0",
-                author: "Myoun",
+                author: "MahMUD",
                 countDown: 5,
                 role: 0,
                 description: {
